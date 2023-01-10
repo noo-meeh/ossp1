e61c03244e48c96d6750b57f976bb7b28e6648d6
Super <Super@DESKTOP-REA3C08>
2023-01-10 17:59:05 +0900

    버튼 추가_2

---
+++ Dynamic_Beat/src/dynamic_beat_4/DynamicBeat.java
@@ -0,0 +1,57 @@
+package dynamic_beat_4;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DynamicBeat extends JFrame {
+
+	private Image screenImage;
+	private Graphics screenGraphic;
+
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+	private JButton exitButton = new JButton(new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png")));
+
+	public DynamicBeat() {
+		setUndecorated(true);
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false); // 한번 만들어진 창 크기 조절 불가
+		setLocationRelativeTo(null); // 실행 시 창 중앙으로 설정
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+
+		menuBar.setBounds(0, 0, 1280, 30);
+		add(menuBar);
+
+		exitButton.setBounds(50, 50, 30, 30);
+		add(exitButton);
+
+		Music introMusic = new Music("introMusic.mp3", true);
+		introMusic.start();
+
+	}
+
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+
+	}
+
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		paintComponents(g); // 이미지를 그려주는 역할
+		this.repaint();
+	}
+
+}
