72c443fd03834c1eccc93b82b0d17ec1181465a6
Super <Super@DESKTOP-REA3C08>
2023-01-10 20:02:35 +0900

    종료버튼 효과 추가

--- Dynamic_Beat/src/dynamic_beat_4/DynamicBeat.java
+++ Dynamic_Beat/src/dynamic_beat_4/DynamicBeat.java
@@ -3,6 +3,9 @@ package dynamic_beat_4;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -16,8 +19,15 @@ public class DynamicBeat extends JFrame {
 
 	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
-	private JButton exitButton = new JButton(new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png")));
-
+	
+	
+	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
+	
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	
+	private int mouseX, mouseY;
+	
 	public DynamicBeat() {
 		setUndecorated(true);
 		setTitle("Dynamic Beat");
@@ -28,15 +38,46 @@ public class DynamicBeat extends JFrame {
 		setVisible(true);
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
-
-		menuBar.setBounds(0, 0, 1280, 30);
-		add(menuBar);
-
-		exitButton.setBounds(50, 50, 30, 30);
+		
+		exitButton.setBounds(1245, 50, 30, 30);
 		//exitButton.setBorderPainted(false);
 		//exitButton.setContentAreaFilled(false);
 		//exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnteredImage);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage);
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+			}
+		});
 		add(exitButton);
+		
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getXOnScreen();
+				setLocation(x - mouseX, y - mouseY); 
+			}
+		});
+		add(menuBar);
+
+
 
 		Music introMusic = new Music("introMusic.mp3", true);
 		introMusic.start();
