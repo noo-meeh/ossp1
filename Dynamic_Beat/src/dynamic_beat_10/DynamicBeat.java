71f37aff77de75c05f025592a153d5b7f6d11609
Super <Super@DESKTOP-REA3C08>
2023-01-14 23:13:40 +0900

    노트 이미지 출력확인

--- Dynamic_Beat/src/dynamic_beat_10/DynamicBeat.java
+++ Dynamic_Beat/src/dynamic_beat_10/DynamicBeat.java
@@ -22,10 +22,12 @@ public class DynamicBeat extends JFrame {
 	
 	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage();
 	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage();
-	private Image noteRouteImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage();
-
+	private Image noteRouteImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage();
+	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage();
+	
 	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
-	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/noteRouteImage.png")));
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
 	
 
 			
@@ -382,8 +384,38 @@ public class DynamicBeat extends JFrame {
 		}
 		
 		if(isGameScreen) {
+			g.drawImage(noteRouteLineImage, 224, 30, null);
+			g.drawImage(noteRouteLineImage, 328, 30, null);
+			g.drawImage(noteRouteLineImage, 432, 30, null);
+			g.drawImage(noteRouteLineImage, 536, 30, null);
+			g.drawImage(noteRouteLineImage, 740, 30, null);
+			g.drawImage(noteRouteLineImage, 844, 30, null);
+			g.drawImage(noteRouteLineImage, 948, 30, null);
+			g.drawImage(noteRouteLineImage, 1052, 30, null);
+			
+			
+			g.drawImage(noteRouteImage, 228, 30, null);
+			g.drawImage(noteRouteImage, 332, 30, null);
+			g.drawImage(noteRouteImage, 436, 30, null);
+			g.drawImage(noteRouteImage, 540, 30, null);
+			g.drawImage(noteRouteImage, 640, 30, null);
+			g.drawImage(noteRouteImage, 744, 30, null);
+			g.drawImage(noteRouteImage, 848, 30, null);
+			g.drawImage(noteRouteImage, 952, 30, null);
+			
+			g.drawImage(noteBasicImage, 228, 120, null);
+			g.drawImage(noteBasicImage, 332, 580, null);
+			g.drawImage(noteBasicImage, 436, 500, null);
+			g.drawImage(noteBasicImage, 540, 340, null);
+			g.drawImage(noteBasicImage, 640, 340, null);
+			g.drawImage(noteBasicImage, 744, 325, null);
+			g.drawImage(noteBasicImage, 848, 305, null);
+			g.drawImage(noteBasicImage, 952, 305, null);
+			
+			
 			g.drawImage(gameInfoImage, 0, 660, null);
 			g.drawImage(judgementLineImage, 0,580, null);
+			
 		}
 		paintComponents(g); // 이미지를 그려주는 역할
 		this.repaint();
