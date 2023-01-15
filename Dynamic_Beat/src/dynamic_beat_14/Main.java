640391fa2263237eefbbc0e19d853f93f7b1ed7b
Super <Super@DESKTOP-REA3C08>
2023-01-16 00:19:47 +0900

    SLEEP_TIME 속도 조절

--- Dynamic_Beat/src/dynamic_beat_14/Main.java
+++ Dynamic_Beat/src/dynamic_beat_14/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static final int SCREEN_WIDTH = 1280; // 넓이
 	public static final int SCREEN_HEIGHT = 720; // 높이
 	public static final int NOTE_SPEED = 7;
-	public static final int SLEEP_TIME = 70;
+	public static final int SLEEP_TIME = 10;
 	
 	
 	public static void main(String[] args) {
