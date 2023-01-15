4f9a2550acc18d5c69259d4008e289ccd44b4e7b
Super <Super@DESKTOP-REA3C08>
2023-01-15 23:17:39 +0900

    screenDraw

--- Dynamic_Beat/src/dynamic_beat_13/Note.java
+++ Dynamic_Beat/src/dynamic_beat_13/Note.java
@@ -1,5 +1,10 @@
 package dynamic_beat_13;
 
-public class Note {
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
+public class Note extends Thread {
+	
+	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage();
 }
